13a9654955ed433db4414bd324206ae134aac2a7
S0yf00d.exe <104031749+S0yf00d-exe@users.noreply.github.com>
2022-04-20 02:42:59 +0000

    Create Bridge_isExit2.sh

---
+++ Bridge/Iceland/Bridge_isExit2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+echo "####################################
+#   ____________________________   #
+#  |  _____                ___  |  #  
+#  | |_   _|__  _ __ _ __ /   | |  #
+#  |   | |/ _ \| '__| '__|  __| |  #
+#  |   | | (_) | |  | | |  (__  |  #
+#  |   |_|\___/|_|  |_|  \____| |  #
+#  |_______                     |  #
+#  | TAiLS | {V.1}------------- |  #
+#  |____________________________|  #
+#                                  # 
+####################################
+_______________________________________
+Author: S0yf00d-exe
+https://github.com/S0yf00d-exe
+_______________________________________
+
+Iceland Exit Nodes
+
+------------------------------"
+# ExitNodes Iceland Unknown StrictNodes 1 
+
+echo 'ExitNodes {is},{??} StrictNodes 1' | tee -a /etc/tor/torrc
+
+#                              
+echo "Restarting Tor Service"
+
+# Restart Tor Service
+
+systemctl restart tor.service
+
+#
+echo "Have A Good Time Surfing Tor"
