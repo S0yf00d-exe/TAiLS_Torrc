b6cf1952614badd6e30c18314fb9efe84e30829d
S0yf00d.exe <104031749+S0yf00d-exe@users.noreply.github.com>
2022-05-05 08:41:06 +0000

    Create MexExit_Bride1.sh

---
+++ Bridge/Mexico/MexExit_Bride1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+echo "####################################
+#   ____________________________   #
+#  |  _____                ___  |  #  
+#  | |_   _|__  _ __ _ __ /   | |  #
+#  |   | |/ _ \| '__| '__|  __| |  #
+#  |   | | (_) | |  | | |  (__  |  #
+#  |   |_|\___/|_|  |_|  \____| |  #
+#  |_______                     |  #
+#  | TAiLS | {V.1}------------- |  #
+#  |____________________________|  #
+#                                  # 
+####################################
+_______________________________________
+Author: S0yf00d-exe
+https://github.com/S0yf00d-exe
+_______________________________________
+*Exclude Unknown Nodes.
+
+*Mexico ExitNodes
+------------------------------"
+
+# Exclude Unknown Nodes.
+
+echo 'ExcludeNodes {??}' | tee -a /etc/tor/torrc
+
+#
+echo "ExitNodes StrictNodes 1 (Every Country Except EcludeNodes List)"
+
+# ExitNodes StrictNodes 1 (Every Country Except EcludeNodes List)
+
+echo 'ExitNodes {mx} StrictNodes 1' | tee -a /etc/tor/torrc
+
+#                              
+echo "Restarting Tor Service"
+
+# Restart Tor Service
+
+systemctl restart tor.service
+
+#
+echo "Have A Good Time Surfing Tor"
